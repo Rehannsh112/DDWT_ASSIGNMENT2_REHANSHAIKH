0770dc5a05c440ef78b5ac97a9ce19cab91e1376
rehannsh112 <rehannsh112@gmail.com>
2024-01-16 12:36:20 +0530

    my mushroom assignment part 2

---
+++ Assignment2/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Mushroom Families";
+            return View();
+        }
+
+        public ActionResult Mushroom()
+        {
+            ViewBag.Title = "Mushroom Details";
+            return View();
+        }
+
+        public ActionResult RecordMushroom()
+        {
+            ViewBag.Title = "Record Mushroom";
+            return View();
+        }
+
+        public ActionResult Modify()
+        {
+            ViewBag.Title = "Modify the Details";
+            return View();
+        }
+    }
+}
